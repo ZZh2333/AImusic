13d19998e9cf6104b3562610dc4965713c3f7b99
ZZh2333 <13164108610@163.com>
2021-10-22 17:46:14 +0800

    flask-mvc框架

--- application.py
+++ application.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
+import os
+from common.libs.UrlManager import UrlManager
+
 
 class Application(Flask):
-    def __init__(self,import_name):
-        super(Application,self).__init__(import_name)
+    def __init__(self, import_name, template_folder=None, root_path=None):
+        super(Application, self).__init__(
+            import_name, template_folder=template_folder, static_folder=None, root_path=root_path)
         self.config.from_pyfile('config/base_setting.py')
         self.config.from_pyfile('config/local_setting.py')
 
         db.init_app(self)
 
+
 db = SQLAlchemy()
-app = Application(__name__)
+app = Application(__name__, template_folder=os.getcwd() +
+                  '/web/templates', root_path=os.getcwd())
 manager = Manager(app)
+
+
+app.add_template_global(UrlManager.buildStaticUrl, "buildStaticUrl")
+app.add_template_global(UrlManager.buildUrl, "buildUrl")
--- manager.py
+++ manager.py
@@ -1,12 +1,14 @@
 from logging import debug
-from application import app,manager
+from application import app, manager
 import traceback
 import sys
 from flask_script import Server
 import www
 
 
-manager.add_command("runlocal",Server(host="0.0.0.0",port=app.config['SERVER_PORT'],use_debugger=True,use_reloader=True))
+manager.add_command("runserver", Server(
+    host="0.0.0.0", port=app.config['SERVER_PORT'], use_debugger=True, use_reloader=True))
+
 
 def main():
     manager.run()
