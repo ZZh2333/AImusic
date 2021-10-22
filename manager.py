2a1c7807c63fa43ae740f5dec728637b1270c372
ZZh2333 <13164108610@163.com>
2021-10-22 16:40:05 +0800

    flask-mvc框架

---
+++ manager.py
@@ -0,0 +1,19 @@
+from logging import debug
+from application import app,manager
+import traceback
+import sys
+from flask_script import Server
+import www
+
+
+manager.add_command("runlocal",Server(host="0.0.0.0",port=app.config['SERVER_PORT'],use_debugger=True,use_reloader=True))
+
+def main():
+    manager.run()
+
+
+if __name__ == '__main__':
+    try:
+        sys.exit(main())
+    except Exception as e:
+        traceback.print_exc()
---
+++ www.py
@@ -0,0 +1,6 @@
+from web.controllers.index import route_index
+from application import app
+
+
+
+app.register_blueprint(route_index,url_prefix = "/")
