2a455fdcecc0e6d4c5e8cf8452d4b97c805c6546
ZZh2333 <13164108610@163.com>
2021-11-03 15:54:13 +0800

    配置uwsgi

--- manager.py
+++ manager.py
@@ -6,8 +6,8 @@ from flask_script import Server
 import www
 
 
-manager.add_command("runserver", Server(
-    host="0.0.0.0", port=app.config['SERVER_PORT'], use_debugger=True, use_reloader=True))
+manager.add_command("run", Server(host="127.0.0.1", port=app.config['SERVER_PORT'], use_debugger=True, use_reloader=True))
+manager.add_command("runserver", Server(host="0.0.0.0", port=6005, use_debugger=True, use_reloader=True))
 
 
 def main():
